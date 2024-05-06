e4dc333b90af8c9926d64aaa3228a6acab0cff8b
Tushar Bhowal <tusharbhowal3211@gmail.com>
2024-05-07 01:12:52 +0530

    some fixes

--- middleware.ts
+++ middleware.ts
@@ -14,5 +14,4 @@ export default clerkMiddleware((auth, req) => {
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
-  //ignoredRoutes: ["/api/webhooks/clerk"],
 };
