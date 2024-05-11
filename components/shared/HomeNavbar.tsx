231b06e5bfc3b72fcee341f19146d3efce3210d2
Tushar Bhowal <tusharbhowal3211@gmail.com>
2024-05-11 21:36:12 +0530

    complete the home page navbar complete project

--- components/shared/HomeNavbar.tsx
+++ components/shared/HomeNavbar.tsx
@@ -1,60 +1,120 @@
-import React from "react";
-import { homeNavLinks } from "../../constants"; // Adjust the path as necessary
+"use client";
+import React, { useState } from "react";
+import { homeNavLinks } from "../../constants";
 import Image from "next/image";
 import { Button } from "../ui/button";
 import Link from "next/link";
 
 const HomeNavbar = () => {
   const filteredLinks = homeNavLinks.filter((link) => link.label !== "Main");
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isMenuClicked, setIsMenuClicked] = useState(false);
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+    setIsMenuClicked(true);
+  };
+
   return (
-    <nav className="bg-black fixed w-full z-20 top-0 start-0">
+    <nav
+      className={`home-nav fixed w-full z-20 top-0 start-0 ${
+        isMenuOpen ? "bg-white " : "bg-black"
+      }`}
+    >
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <Link href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+        <Link
+          href="/"
+          className="flex items-center space-x-3 rtl:space-x-reverse"
+        >
           <Image
             src="/assets/images/logo.png"
             alt="logo"
             width={40}
             height={30}
           />
-          <span className="self-center text-xl font-semibold whitespace-nowrap text-white">
+          <span
+            className={`self-center text-xl font-semibold whitespace-nowrap ${
+              isMenuOpen ? "text-black" : "text-white"
+            }`}
+          >
             SimplifyEfforts
           </span>
-              </Link>
-              
-
-        <div className="md:flex md:order-2 hidden space-x-3 rtl:space-x-reverse">
-          <Button
-            variant="ghost"
-            className="text-white border border-white rounded-full w-24 hover:border-black"
-          >
-            <Link href="/sign-in">Sign In</Link>
-          </Button>
-          <Button variant="outline" className="rounded-full w-24">
-            <Link href="/sign-up">Sign Up</Link>
-          </Button>
-              </div>
-              
+        </Link>
         <div
-          className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+          className={`items-center justify-between w-full md:flex md:w-auto md:order-1 ${
+            isMenuOpen ? "flex flex-col items-center text-black" : "hidden"
+          }`}
           id="navbar-sticky"
         >
-          <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border rounded-lg  md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0">
+          <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium rounded-lg md:space-x-8  md:flex-row md:mt-0">
             {filteredLinks.map((link) => (
               <li key={link.label}>
                 <a
                   href={link.route}
-                  className="block py-2 px-3 text-gray-400 rounded  md:hover:bg-transparent md:hover:text-indigo-500 md:p-0"
+                  className={`block py-2 px-3  rounded hover:bg-transparent hover:text-indigo-500 md:p-0 ${
+                    isMenuOpen ? "text-black" : "text-gray-400"
+                  }`}
                 >
                   {link.label}
                 </a>
               </li>
             ))}
           </ul>
-              </div>
-              <div className="md:hidden">
-                  
-              </div>
-
+        </div>
+        <div
+          className={`md:flex md:order-2 space-x-3 ${
+            isMenuOpen
+              ? "flex flex-row justify-center items-center text-black w-full"
+              : "hidden"
+          } justify-center items-center`} // Added justify-center items-center
+        >
+          <Button
+            variant="ghost"
+            className={`border rounded-full w-24 ${
+              isMenuOpen
+                ? "text-black border-black"
+                : "text-white border-white hover:border-black"
+            }`}
+          >
+            <Link href="/sign-in">Sign In</Link>
+          </Button>
+          <Button
+            variant="outline"
+            className={`border rounded-full w-24 ${
+              isMenuOpen
+                ? "text-white bg-black"
+                : "text-black border-white hover:border-black"
+            }`}
+          >
+            <Link href="/sign-up">Sign Up</Link>
+          </Button>
+        </div>
+        <div className="md:hidden order-1">
+          <button
+            className={`${
+              isMenuOpen ? "bg-black" : "bg-white"
+            } p-2 rounded-full absolute top-3 right-2 menu-button ${
+              isMenuClicked ? "top-0" : "top-3"
+            }`}
+            onClick={toggleMenu}
+          >
+            {isMenuOpen ? (
+              <Image
+                src="/assets/icons/homecross.svg"
+                alt="close"
+                width={24}
+                height={24}
+              />
+            ) : (
+              <Image
+                src="/assets/icons/homemenu.svg"
+                alt="menu"
+                width={24}
+                height={24}
+                className=""
+              />
+            )}
+          </button>
+        </div>
       </div>
     </nav>
   );
